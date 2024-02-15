175a4e4b5e1e4d5d08fe9360c2d5e4a50986d9b3
hr203 <hr203@sussex.ac.uk>
2024-02-15 15:48:00 -0800

    helloworld added

---
+++ phys/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+
+extern "C" void hellworld();
+
+using std::bind;
+
+void helloworld() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return;
+}
